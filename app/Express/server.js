c33fa74716eb8bedb576f728d03861090effc845
IF <ksa.if.reg@mail.ru>
2015-07-09 12:13:35 +0300

    добавили папку app, переместили туда приложение

---
+++ app/Express/server.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+var express = require("express"),
+http = require("http"),
+app = express(),
+toDos = {[{
+"description" : "Купить продукты",
+"tags" : [ "шопинг", "рутина" ]
+},{
+"description" : "Сделать несколько новых задач",
+"tags" : [ "писательство", "работа" ]
+},{
+"description" : "Подготовиться к лекции в понедельник",
+"tags" : [ "работа", "преподавание" ]
+},{
+"description" : "Ответить на электронные письма",
+"tags" : [ "работа" ]
+},{
+"description" : "Вывести Грейси на прогулку в парк",
+"tags" : [ "рутина", "питомцы" ]
+},{
+"description" : "Закончить писать книгу",
+"tags" : [ "писательство", "работа" ]
+}]};
+
+app.use(express.static(__dirname + "/client"));
+http.createServer(app).listen(3000);
+// этот маршрут замещает наш файл
+// todos.json в примере из главы 5
+app.get("todos.json", function (req, res) {
+res.json(toDos);
+});
